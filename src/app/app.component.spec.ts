0314cd20375199c652ff100760c425fa3469a4b0
Vlad <vlad.mezhevyi@gmail.com>
2025-04-05 23:41:31 +0100

    fix(app.component.spec.ts): add missing field for googleMapsServiceSpy

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { GoogleMapsService } from './feature/maps/services/google-maps.service';
+import { signal } from '@angular/core';
 
 describe('AppComponent', () => {
   let fixture: ComponentFixture<AppComponent>;
   const googleMapsServiceSpy = jasmine.createSpyObj('GoogleMapsService', ['loadGoogleMaps']);
-
+  googleMapsServiceSpy.isGoogleMapsLoaded = signal<boolean>(false);
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
