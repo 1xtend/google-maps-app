2b33f492f747a95f17fad75efcdc298edbd51744
Vlad <vlad.mezhevyi@gmail.com>
2025-04-09 20:43:14 +0100

    fix(deployment): change environment object in generate-env.js

--- scripts/generate-env.js
+++ scripts/generate-env.js
@@ -3,9 +3,10 @@ const path = require('path');
 
 const envFilePath = path.join(__dirname, '../src/environments/environment.ts');
 
-const fileContent = `export const environment = {
-  production: true,
-  apiKey: '${ process.env.GOOGLE_API_KEY }'
+const fileContent = `export const environment =  {
+  production: false,
+  googleMapsApiKey: '${ process.env.GOOGLE_API_KEY }',
+  placesApiUrl: 'https://failteireland.azure-api.net/opendata-api/v2/attractions'
 };
 `;
 
