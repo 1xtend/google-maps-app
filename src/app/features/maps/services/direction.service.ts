a3e2b87654d6ea9a0750e271df91c56705f88709
Vlad <vlad.mezhevyi@gmail.com>
2025-04-10 13:57:10 +0100

    refactor(common): move displaying direction logic into own method

--- src/app/features/maps/services/direction.service.ts
+++ src/app/features/maps/services/direction.service.ts
@@ -6,19 +6,13 @@ import { Injectable } from '@angular/core';
 export class DirectionService {
   private directionsRenderer: google.maps.DirectionsRenderer | null = null;
 
-  calculateDirection(map: google.maps.Map | undefined, origin: google.maps.LatLngLiteral, destination: google.maps.LatLngLiteral, travelMode: google.maps.TravelMode) {
+  calculateDirection(map: google.maps.Map | undefined, origin: google.maps.LatLngLiteral, destination: google.maps.LatLngLiteral, travelMode: google.maps.TravelMode): void {
     this.clearDirection();
 
     const directionService = new google.maps.DirectionsService();
     this.directionsRenderer = new google.maps.DirectionsRenderer({ map });
 
-    directionService.route({ origin, destination, travelMode }, (response, status) => {
-      if (status !== google.maps.DirectionsStatus.OK) {
-        return;
-      }
-
-      this.directionsRenderer?.setDirections(response);
-    })
+    this.displayDirection(directionService, { origin, destination, travelMode });
   }
 
   clearDirection(): void {
@@ -28,5 +22,15 @@ export class DirectionService {
 
     this.directionsRenderer.setMap(null);
     this.directionsRenderer = null;
+  }
+
+  private displayDirection(directionService: google.maps.DirectionsService, request: google.maps.DirectionsRequest): void {
+    directionService.route(request, (response, status) => {
+      if (status !== google.maps.DirectionsStatus.OK) {
+        return;
+      }
+
+      this.directionsRenderer?.setDirections(response);
+    });
   }
 }
